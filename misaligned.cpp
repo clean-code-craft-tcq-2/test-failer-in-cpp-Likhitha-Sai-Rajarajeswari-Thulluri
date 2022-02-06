57fc35676e05b3d783a56042042bed46bd3246c3
Likhitha-Sai-Rajarajeswari-Thulluri <91083096+Likhitha-Sai-Rajarajeswari-Thulluri@users.noreply.github.com>
2022-02-06 23:09:09 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
 #include <assert.h>
-
+#include<string.h>
 
 const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+void formatString(int i, int j)
+{
+    string st = (i*5+j).to_string()+ " | "+ majorColor[i] + " | " + minorColor[j];
+    
+}
 
 int printColorMap() {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[j] << "\n";
+            //std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[j] << "\n";
+            std::cout<<formatString(i,j);
         }
     }
     return i * j;
 }
 
+void testPrintColorMap()
+{
+    assert(formatString(0,0) == "0 ! White | Blue");
+    assert(formatString(2,3) == "13 ! Black ! Brown);
+}
+
 int main() {
     int result = printColorMap();
     assert(result == 25);
+    testPrintColorMap();
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
